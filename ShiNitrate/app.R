0ed460373cc4eb4e4bad4e27f0a868774419fc47
massambaagro <massambaagro@gmail.com>
2024-09-30 01:25:47 +0200

    Script ShinyTrate avec les analyses que j'ai intégrées + Script analyses temporelles

--- ShiNitrate/app.R
+++ ShiNitrate/app.R
@@ -8,47 +8,92 @@ library(leaflet)
 
 
 # Dataset
-quali_nitrate <- read.csv("/Users/massambadiop/Documents/INSTITUT AGRO/M2 Sciences des données/Analyse de données massives sur R/Projet Shiny/Projet-R---shiny---2024--Agrocampus-main/qualite-des-cours-deau-vis-a-vis-des-nitrates-en-bretagne.csv",stringsAsFactors  = T)
-df_filtre <- subset(quali_nitrate, nom_sage == "Couesnon") # Sélection d'une localité (Couesnon)
 
 
-# Define UI
-ui <- fluidPage(
-  tags$h1("ShiNitrate Project", 
-          style = "color: #FF5733; text-align: center; font-weight: bold; 
-                     font-size: 32px; margin-top: 20px;"),
-  
-  tags$h3("Get insights about nitrates!", 
-          style = "color: #555; text-align: center; font-style: italic; 
-                     margin-top: -10px; margin-bottom: 30px;"),
-  
- 
-  
-  # Sidebar layout with input and output definitions
-  sidebarLayout(
-    sidebarPanel(
-      h3("Stations de Couesnon"),
-      p("Cette carte est interactive. Vous pouvez faire un zoom avant/arrière, faire glisser la carte et cliquer sur les marqueurs.")
-    ),
-    
-    # Main panel for displaying the map
-    mainPanel(
-      leafletOutput("map", height = 600)
-    )
-  )
+
+# Load necessary libraries
+library(shiny)
+library(ggfortify)  # For autoplot
+library(plotly)     # For ggplotly
+
+# Define the UI
+ui <- navbarPage("Interactive Time Series App",
+                 
+                 
+                 # Page 2
+                 tabPanel("Evolutif de la concentration en nitrates",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput("column2", "Select Column for Page 2:", choices = NULL)
+                            ),
+                            mainPanel(
+                              plotlyOutput("plot2")
+                            )
+                          )
+                 ),
+                 
+                 # Page 3
+                 tabPanel("Page 3",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput("column3", "Select Column for Page 3:", choices = NULL)
+                            ),
+                            mainPanel(
+                              plotlyOutput("plot3")
+                            )
+                          )
+                 ),
+                 
+                 # Page 4
+                 tabPanel("Page 4",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput("column4", "Select Column for Page 4:", choices = NULL)
+                            ),
+                            mainPanel(
+                              plotlyOutput("plot4")
+                            )
+                          )
+                 )
 )
 
-# Define server logic
+# Define the server logic
 server <- function(input, output, session) {
   
-  # Create the Leaflet map
-  output$map <- renderLeaflet({
-    leaflet(df_filtre) %>%
-      addTiles() %>%  # Ajouter la couche de tuiles
-      addCircles(lng = ~longitude, lat = ~latitude, color = "blue", fillOpacity = 0.7, radius = 200) %>% 
-      addPopups(lng = ~longitude, lat = ~latitude, popup = ~nom_cours_eau)
+  data <- read.csv("/Users/massambadiop/Documents/INSTITUT AGRO/M2 Sciences des données/Analyse de données massives sur R/Projet-R---shiny---2024--Agrocampus-1/qualite-des-cours-deau-vis-a-vis-des-nitrates-en-bretagne.csv",stringsAsFactors  = T, header = T)
+  data.ts <- ts(data, start = 1995, end = 2023, frequency = 1)
+  
+  # Update choices for column selection in all pages
+  observe({
+    updateSelectInput(session, "column1", choices = colnames(data))
+    updateSelectInput(session, "column2", choices = colnames(data))
+    updateSelectInput(session, "column3", choices = colnames(data))
+    updateSelectInput(session, "column4", choices = colnames(data))
+  })
+  
+
+  
+  # Render Plot for Page 2
+  output$plot2 <- renderPlotly({
+    req(input$column2)
+    plot <- autoplot(data.ts[, input$column2], xlab = "Year", ylab = input$column2, main = paste("Time Series for", input$column2))
+    ggplotly(plot)
+  })
+  
+  # Render Plot for Page 3
+  output$plot3 <- renderPlotly({
+    req(input$column3)
+    plot <- autoplot(data.ts[, input$column3], xlab = "Year", ylab = input$column3, main = paste("Time Series for", input$column3))
+    ggplotly(plot)
+  })
+  
+  # Render Plot for Page 4
+  output$plot4 <- renderPlotly({
+    req(input$column4)
+    plot <- autoplot(data.ts[, input$column4], xlab = "Year", ylab = input$column4, main = paste("Time Series for", input$column4))
+    ggplotly(plot)
   })
 }
 
 # Run the Shiny app
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
