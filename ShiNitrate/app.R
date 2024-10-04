278e0ed4a690b3b18c04e0864eedd465958779ec
massambaagro <massambaagro@gmail.com>
2024-10-05 00:47:15 +0200

    Map intégré dans Shiny + Refining Shiny app

--- ShiNitrate/app.R
+++ ShiNitrate/app.R
@@ -1,99 +1,224 @@
+# Installer les packages si nécessaire
 # install.packages("shiny")
 # install.packages("leaflet")
+# install.packages("ggfortify")
+# install.packages("bslib")
+install.packages("shinythemes")
 
-
-# Load required libraries
+# Charger les bibliothèques nécessaires
 library(shiny)
+library(shinythemes)
+library(bslib)
 library(leaflet)
+library(ggfortify)  # Pour autoplot (séries temporelles)
+library(plotly)     # Pour ggplotly (graphique interactif)
 
+# Charger les données
+data_path <- "/Users/massambadiop/Documents/INSTITUT AGRO/M2 Sciences des données/Analyse de données massives sur R/Projet-R---shiny---2024--Agrocampus-1/qualite-des-cours-deau-vis-a-vis-des-nitrates-en-bretagne.csv"
 
-# Dataset
+if (file.exists(data_path)) {
+  data <- read.csv(data_path, stringsAsFactors = TRUE)
+  
+  # Assurer que seules les colonnes numériques sont converties en séries temporelles
+  numeric_cols <- sapply(data, is.numeric)
+  data.ts <- ts(data[, numeric_cols], start = 1995, end = 2023, frequency = 1)
+} else {
+  stop("Le fichier de données est introuvable. Vérifiez le chemin.")
+}
 
+# Identifier les régions, départements et années
+regions <- unique(data$libelle_region)
+departements <- split(data$libelle_departement, data$libelle_region)
+annees <- sort(unique(data$annee))  # Trier les années
 
+# Définir l'interface utilisateur (UI)
+ui <- fluidPage(theme = shinytheme("cerulean"),
+  titlePanel("ShinyTrate Project"), 
+  navbarPage("Application Interactive des Séries Temporelles",
+             
+             # Page 1 - Évolution de la concentration en nitrates
+             tabPanel("Evolutif de la concentration en nitrates",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("column1", "Select Column for Page 1:", choices = NULL)
+                        ),
+                        mainPanel(
+                          plotlyOutput("plot1"),  # Graphe 1
+                          hr(),  # Ligne horizontale
+                          plotlyOutput("nitratePlot_dep"),  # Graphe 2
 
-# Load necessary libraries
-library(shiny)
-library(ggfortify)  # For autoplot
-library(plotly)     # For ggplotly
-
-# Define the UI
-ui <- navbarPage("Interactive Time Series App",
-                 
-                 
-                 # Page 2
-                 tabPanel("Evolutif de la concentration en nitrates",
-                          sidebarLayout(
-                            sidebarPanel(
-                              selectInput("column2", "Select Column for Page 2:", choices = NULL)
-                            ),
-                            mainPanel(
-                              plotlyOutput("plot2")
-                            )
-                          )
-                 ),
-                 
-                 # Page 3
-                 tabPanel("Page 3",
-                          sidebarLayout(
-                            sidebarPanel(
-                              selectInput("column3", "Select Column for Page 3:", choices = NULL)
-                            ),
-                            mainPanel(
-                              plotlyOutput("plot3")
-                            )
-                          )
-                 ),
-                 
-                 # Page 4
-                 tabPanel("Page 4",
-                          sidebarLayout(
-                            sidebarPanel(
-                              selectInput("column4", "Select Column for Page 4:", choices = NULL)
-                            ),
-                            mainPanel(
-                              plotlyOutput("plot4")
-                            )
-                          )
-                 )
+                        )
+                      )
+             ),
+             tabPanel("Evolution de la concentration en nitrates par Département",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("department", 
+                                      "Select Departement:", 
+                                      choices = unique(nitrate_trend_dept$libelle_departement), 
+                                      selected = unique(nitrate_trend_dept$libelle_departement)[1])
+                        ),
+                        
+                        mainPanel(
+                          plotOutput("nitratePlot")
+                        )
+                      )
+             ),
+             
+             # Page 3 - Carte des prélèvements par région, département et année
+             tabPanel("Cartographie",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectizeInput("region", "Sélectionnez une Région :", 
+                                         choices = regions, 
+                                         selected = "Bretagne", 
+                                         options = list(maxItems = 1)),  # Sélection par défaut
+                          uiOutput("departementUI"),  # UI dynamique pour le choix des départements
+                          sliderInput("annee", "Sélectionnez une Année :", 
+                                      min = min(annees), 
+                                      max = max(annees), 
+                                      value = max(annees),  # Valeur par défaut : année maximale
+                                      step = 1,
+                                      ticks = FALSE),  # Ne pas afficher les ticks
+                          radioButtons("metrique", "Choisissez la Métrique :", 
+                                       choices = c("Concentration Moyenne" = "concentration_moy", 
+                                                   "Q90" = "valeur_q90"),  # Options de métrique
+                                       selected = "concentration_moy", 
+                                       inline = TRUE)  # Afficher les boutons radio en ligne
+                        ),
+                        mainPanel(
+                          leafletOutput("map", height = 600)  # Carte interactive
+                        )
+                      )
+             )
+  )
 )
 
-# Define the server logic
+# Définir la logique serveur
 server <- function(input, output, session) {
   
-  data <- read.csv("/Users/massambadiop/Documents/INSTITUT AGRO/M2 Sciences des données/Analyse de données massives sur R/Projet-R---shiny---2024--Agrocampus-1/qualite-des-cours-deau-vis-a-vis-des-nitrates-en-bretagne.csv",stringsAsFactors  = T, header = T)
-  data.ts <- ts(data, start = 1995, end = 2023, frequency = 1)
-  
-  # Update choices for column selection in all pages
+  # Mettre à jour les choix de colonne pour chaque page
   observe({
-    updateSelectInput(session, "column1", choices = colnames(data))
-    updateSelectInput(session, "column2", choices = colnames(data))
-    updateSelectInput(session, "column3", choices = colnames(data))
-    updateSelectInput(session, "column4", choices = colnames(data))
+    # Assurez-vous que les colonnes sélectionnées existent dans le data.frame
+    updateSelectInput(session, "column1", choices = colnames(data[,20:26]))
   })
   
-
-  
-  # Render Plot for Page 2
-  output$plot2 <- renderPlotly({
-    req(input$column2)
-    plot <- autoplot(data.ts[, input$column2], xlab = "Year", ylab = input$column2, main = paste("Time Series for", input$column2))
+  ### Render Plot for Page 1
+  # Graphe 1
+  output$plot1 <- renderPlotly({
+    req(input$column1)
+    plot <- autoplot(data.ts[, input$column1],type= 'b', xlab = "Année", ylab = input$column1,colour='red', main = paste("Evolution globale de la", input$column1,"en nitrate" ))
     ggplotly(plot)
   })
   
-  # Render Plot for Page 3
-  output$plot3 <- renderPlotly({
-    req(input$column3)
-    plot <- autoplot(data.ts[, input$column3], xlab = "Year", ylab = input$column3, main = paste("Time Series for", input$column3))
-    ggplotly(plot)
+  # Graphe 2
+  output$nitratePlot_dep <- renderPlotly({
+    # S'assurer que nitrate_trend_dept est défini
+    req(input$column1)
+    
+    ggplotly(ggplot(nitrate_trend_dept, aes(x = annee, y = mean_concentration, color = libelle_departement)) +
+               geom_line(size=0.9) + 
+               geom_point(size=1) +
+               labs(title = "Evolution de la Concentration moyenne en nitrates par Département",
+                    x = NULL,
+                    y = "Concentration moyenne en nitrates (mg/L)",
+                    color = "Départements") +
+               theme_minimal() +
+               theme(legend.position = "right",
+                     plot.title = element_text(size = 16),
+                     axis.title.x = element_text(size = 14),
+                     axis.title.y = element_text(size = 14),
+                     legend.text = element_text(size = 12))
+    )
+  })
+  observe({
+    # Assurez-vous que les colonnes sélectionnées existent dans le data.frame
+    updateSelectInput(session, "column1", choices = colnames(data[,20:22]))
+  })
+  # Graphe 3
+  output$nitratePlot <- renderPlot({
+    req(input$department, nitrate_trend_dept)
+    
+    dept_data <- nitrate_trend_dept %>% 
+      filter(libelle_departement == input$department)
+    
+    ggplot(dept_data, aes(x = annee, y = mean_concentration)) +
+               geom_line(color = "blue", size=2) +
+               geom_point(color = "red", size=2) +
+               labs(title = paste("Evolution de la Concentration en nitrate en", input$department),
+                    x = "Year",
+                    y = "Average Nitrate Concentration (mg/L)") +
+               theme_gray()
   })
   
-  # Render Plot for Page 4
-  output$plot4 <- renderPlotly({
-    req(input$column4)
-    plot <- autoplot(data.ts[, input$column4], xlab = "Year", ylab = input$column4, main = paste("Time Series for", input$column4))
-    ggplotly(plot)
+  # UI dynamique pour le choix des départements
+  output$departementUI <- renderUI({
+    req(input$region)  # Assurer que la région est sélectionnée
+    selectizeInput("departement", "Sélectionnez un Département :", 
+                   choices = departements[[input$region]], 
+                   selected = "Finistère",  # Sélection par défaut
+                   options = list(maxItems = 1, placeholder = "Choisissez un département..."))
+  })
+  
+  # Initialisation de la carte
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%  # Ajouter les tuiles de base
+      setView(lng = -3.5, lat = 48.5, zoom = 7)  # Centrer sur la Bretagne
+  })
+  
+  # Mise à jour de la carte en fonction des sélections
+  observe({
+    req(input$departement, input$annee)  # Assurer que le département et l'année sont sélectionnés
+    
+    # Filtrer les données pour le département et l'année sélectionnés
+    data_filtered <- subset(data, libelle_departement == input$departement & annee == input$annee)
+    
+    leafletProxy("map") %>%
+      clearMarkers() %>%  # Effacer les marqueurs précédents
+      clearControls()  # Effacer les légendes précédentes
+    
+    if (nrow(data_filtered) > 0) {
+      # Palette de couleurs en fonction de la métrique choisie
+      pal <- colorNumeric(palette = "YlOrRd", domain = data_filtered[[input$metrique]])
+      
+      # Ajouter les marqueurs pour chaque station
+      leafletProxy("map") %>%
+        addCircleMarkers(
+          data = data_filtered,
+          lng = ~longitude, lat = ~latitude,
+          color = pal(data_filtered[[input$metrique]]),
+          fillOpacity = 0.7, radius = 10,
+          label = paste(
+            "Station:", data_filtered$libelle_station, "<br>",
+            "Concentration Moyenne:", data_filtered$concentration_moy, "mg/L", "<br>",
+            "Q90:", data_filtered$valeur_q90, "mg/L", "<br>",
+            "Nombre de Prélèvements:", data_filtered$nombre_prelevements
+          ),
+          labelOptions = labelOptions(
+            direction = 'top', textOnly = TRUE,
+            style = list("color" = "black", "background-color" = "white")
+          ),
+          popup = paste(
+            "Station:", data_filtered$libelle_station, "<br>",
+            "Concentration Moyenne:", data_filtered$concentration_moy, "mg/L", "<br>",
+            "Q90:", data_filtered$valeur_q90, "mg/L", "<br>",
+            "Nombre de Prélèvements:", data_filtered$nombre_prelevements
+          )
+        )
+      
+      # Ajouter une légende pour la métrique choisie
+      leafletProxy("map") %>%
+        addLegend("bottomright", pal = pal, values = data_filtered[[input$metrique]],
+                  title = input$metrique, opacity = 0.7)
+      
+    } else {
+      # Message lorsque les données sont manquantes
+      leafletProxy("map") %>%
+        addMarkers(lng = -2.5, lat = 48.5, popup = "Aucun prélèvement disponible pour ce département et cette année.")
+    }
   })
 }
 
-# Run the Shiny app
+# Exécuter l'application Shiny
 shinyApp(ui = ui, server = server)
